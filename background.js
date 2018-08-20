978f6ec57fa7c62643a61a742d59f2bea5de28c6
Fengyao <1724238345@qq.com>
2018-08-20 14:46:42 +0800

    'commit'

--- background.js
+++ background.js
@@ -1,28 +1,24 @@
 chrome.browserAction.onClicked.addListener(function() {
-  var a = confirm("是否清理缓存？");
-  if (a) {
-    var data = {
-      appcache: true, //则调用执行清除方法
-      cache: true,
-      cookies: false,
-      downloads: false,
-      fileSystems: true,
-      formData: true,
-      history: true,
-      indexedDB: true,
-      localStorage: true,
-      serverBoundCertificates: true,
-      pluginData: true,
-      passwords: false,
-      webSQL: true
-    };
-    var millisecondsPerWeek = 1000 * 60 * 60 * 24 * 30;
-    var ago = new Date().getTime() - millisecondsPerWeek;
-    chrome.browsingData.remove({ since: ago }, data, function() {
-      //弹出框
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.reload(tabs[0].id, function() {});
-      });
+  var data = {
+    appcache: true, 
+    cache: true,
+    cookies: false,
+    downloads: false,
+    fileSystems: true,
+    formData: true,
+    history: true,
+    indexedDB: true,
+    localStorage: true,
+    serverBoundCertificates: true,
+    pluginData: true,
+    passwords: false,
+    webSQL: true
+  };
+  var millisecondsPerWeek = 1000 * 60 * 60 * 24 * 30;
+  var ago = new Date().getTime() - millisecondsPerWeek;
+  chrome.browsingData.remove({ since: ago }, data, function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.reload(tabs[0].id, function() {});
     });
-  }
+  });
 });
